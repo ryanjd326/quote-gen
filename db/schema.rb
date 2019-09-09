197686460c758e1717a26deb192e099978b631cd
Ryan Dickinson <ryanjd326@gmail.com>
2019-09-09 19:33:39 +0000

    added migrations and revised quotes controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_194139) do
+ActiveRecord::Schema.define(version: 2019_09_09_185355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_09_01_194139) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "source"
   end
 
 end
